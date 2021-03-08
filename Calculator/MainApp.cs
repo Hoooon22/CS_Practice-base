4d630293a7423f28ee8df470f470bd8819acbf68
Hoooon22 <momo990305@gmail.com>
2021-03-08 22:10:47 +0900

    First Commit

---
+++ Calculator/MainApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Cal
+{
+    class Calculator
+    {
+        public static int plus(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+
+    class MainApp
+    {
+        public static void Main()
+        {
+            int result = Calculator.plus(3, 4);
+            Console.WriteLine(result);
+
+            result = Calculator.Minus(5, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ RefReturn/MainApp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefReturn
+{
+    class Product
+    {
+        private int price = 100;
+
+        public ref int GetPrice()
+        {
+            return ref price;
+        }
+        public void PrintPrice()
+        {
+            Console.WriteLine($"price :{price}");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Product carrot = new Product();
+            ref int ref_local_price = ref carrot.GetPrice(); // ref를 붙이면 해당 변수 수정시 클래스 내 변수 바뀜
+            int normal_local_price = carrot.GetPrice();
+        }
+    }
+}
