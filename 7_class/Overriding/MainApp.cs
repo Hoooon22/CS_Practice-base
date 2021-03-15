5aa0ae0138406d25f0552d2f91ca8a4f0e21e91f
Hoooon22 <momo990305@gmail.com>
2021-03-15 15:26:08 +0900

    ch07-Overriding

---
+++ 7_class/Overriding/MainApp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overriding
+{
+    class ArmorSuite
+    {
+        public virtual void Initialize()
+        {
+            Console.WriteLine("Armored");
+        }
+    }
+
+    class IronMan : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("Repulsor Rats Armed");
+        }
+    }
+
+    class WarMachine : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("Double-Barrel Cannons Armed");
+            Console.WriteLine("Micro_rocekt Launcher Armed");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Crating ArmorSuite...");
+            ArmorSuite armorSuite = new ArmorSuite();
+            armorSuite.Initialize();
+
+            Console.WriteLine("\nCreating IronMan...");
+            ArmorSuite ironman = new IronMan();
+            ironman.Initialize();
+
+            Console.WriteLine("\nCreating WarMachine");
+            ArmorSuite warmachine = new WarMachine();
+            warmachine.Initialize();
+        }
+    }
+}
