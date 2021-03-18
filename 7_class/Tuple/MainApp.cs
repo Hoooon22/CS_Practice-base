f67f9b4a973466f637547c21bfbd06f7ad621cfc
Hoooon22 <momo990305@gmail.com>
2021-03-18 12:46:57 +0900

    named tuple

--- 7_class/Tuple/MainApp.cs
+++ 7_class/Tuple/MainApp.cs
@@ -10,6 +10,14 @@ namespace Tuple
     {
         static void Main(string[] args)
         {
+            // 명명되지 않은 튜플
+            var a = ("슈퍼맨", 9999);
+            Console.WriteLine($"{a.Item1}, {a.Item2}");
+
+            // 명명된 튜플
+            var b = (Name: "박상현", Age: 17);
+            Console.WriteLine($"{b.Name}. {b.Age}");
+
         }
     }
 }
