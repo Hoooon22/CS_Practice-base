54be43428eb277b16c6c2bfceeb2ba593eacf0df
Hoooon22 <momo990305@gmail.com>
2021-03-19 13:36:03 +0900

    error cs8370

--- 7_class/PositionalPattern/MainApp.cs
+++ 7_class/PositionalPattern/MainApp.cs
@@ -12,8 +12,12 @@ namespace PositionalPattern
         {
             return client switch
             {
-
-            }
+                ("학생", int n) when n < 18 => 0.2,
+                ("학생", _) => 0.1,
+                ("일반", int n) when n < 18 => 0.1,
+                ("일반", _) = > 0.05,
+                _ => 0,
+            };
         }
 
         static void Main(string[] args)
