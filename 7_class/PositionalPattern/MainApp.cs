4c082c086f88dad0db24f7805976f2b0e5003d0b
Hoooon22 <momo990305@gmail.com>
2021-03-19 13:28:56 +0900

    make positional Pattern

---
+++ 7_class/PositionalPattern/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositionalPattern
+{
+    class MainApp
+    {
+        private static double GetDiscountrate(object client)
+        {
+            return client switch
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
