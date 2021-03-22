455185d5af90d1f62a14afe1c9930f2c2e581916
Hoooon22 <momo990305@gmail.com>
2021-03-22 15:09:19 +0900

    start 9_property make Property.cs

---
+++ 9_property/Property/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Property
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
