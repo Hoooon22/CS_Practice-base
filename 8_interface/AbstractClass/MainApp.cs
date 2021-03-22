40af8d9b813559da5b40708530f6432902db9899
Hoooon22 <momo990305@gmail.com>
2021-03-22 15:06:36 +0900

    AbstractClass

--- 8_interface/AbstractClass/MainApp.cs
+++ 8_interface/AbstractClass/MainApp.cs
@@ -2,11 +2,37 @@
 
 namespace AbstractClass
 {
+    abstract class AbstactBase
+    { 
+        protected void PrivatMethodA()
+        {
+            Console.WriteLine("AbstractBase.PrivateMethodA()");
+        }
+
+        public void PublicMethodA()
+        {
+            Console.WriteLine("AbstractBase.PublicMethodA()");
+        }
+
+        public abstract void AbstractMethodA();
+    }
+
+    class Derived : AbstactBase
+    {
+        public override void AbstractMethodA()
+        {
+            Console.WriteLine("Derived.AbstractMethodA()");
+            PrivatMethodA();
+        }
+    }
+
     class MainApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AbstactBase obj = new Derived();
+            obj.AbstractMethodA();
+            obj.PublicMethodA();
         }
     }
 }
