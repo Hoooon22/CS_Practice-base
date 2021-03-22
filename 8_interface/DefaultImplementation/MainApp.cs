8815c1460bf27c2287486364f354662f72ece117
Hoooon22 <momo990305@gmail.com>
2021-03-22 14:12:33 +0900

    make DefaultImplementation

---
+++ 8_interface/DefaultImplementation/MainApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultImplementation
+{
+    interface ILogger
+    {
+        void WriteLog(string message);
+
+        void WriteError(string error)
+        {
+            WriteLog($"Error : {error}");
+
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
