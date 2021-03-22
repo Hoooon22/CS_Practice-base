4fb2ac6ac7b1b75ec9190f86a92ea0abe9265a68
Hoooon22 <momo990305@gmail.com>
2021-03-22 14:58:36 +0900

    make AbstractClass.cs

---
+++ 8_interface/AbstractClass/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
