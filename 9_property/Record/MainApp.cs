bba34115903063274ee74807ae47c9fdb50a8076
Hoooon22 <momo990305@gmail.com>
2021-03-23 16:10:21 +0900

    Record

--- 9_property/Record/MainApp.cs
+++ 9_property/Record/MainApp.cs
@@ -2,11 +2,38 @@
 
 namespace Record
 {
+    record RTransaction
+    {
+        public string From { get; init; }
+        public string To   { get; init; }
+        public int Amount  { get; init; }
+
+        public override string ToString()
+        {
+            return $"{From,-10} -> {To,-10}: ${Amount}";
+        }
+    }
+
     class MainApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RTransaction tr1 = new RTransaction
+            {
+                From = "Alice",
+                To = "Bob",
+                Amount = 100
+            };
+
+            RTransaction tr2 = new RTransaction
+            {
+                From = "Alice", 
+                To = "Charlie", 
+                Amount = 100
+            };
+
+            Console.WriteLine(tr1);
+            Console.WriteLine(tr2);
         }
     }
 }
