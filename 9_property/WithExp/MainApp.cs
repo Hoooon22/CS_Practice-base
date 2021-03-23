e741d1c29f06927c54dd3799d51cd39482c8fa0b
Hoooon22 <momo990305@gmail.com>
2021-03-23 16:28:16 +0900

    WithExp

--- 9_property/WithExp/MainApp.cs
+++ 9_property/WithExp/MainApp.cs
@@ -2,11 +2,29 @@
 
 namespace WithExp
 {
+    record RTansaction
+    {
+        public string From { get; init; }
+        public string To   { get; init; }
+        public int Amount  { get; init; }
+
+        public override string ToString()
+        {
+            return $"{From,-10} -> {To,-10} : ${Amount}";
+        }
+    }
+
     class MainApp
     {
         static void Main(string[] args)
         {
+            RTansaction tr1 = new RTansaction { From = "Alice", To = "Bob", Amount = 100 };
+            RTansaction tr2 = tr1 with { To = "Charlie" };
+            RTansaction tr3 = tr2 with { From = "Dave", Amount = 30 };
 
+            Console.WriteLine(tr1);
+            Console.WriteLine(tr2);
+            Console.WriteLine(tr3);
         }
     }
 }
