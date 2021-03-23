f6bac2b29b0743fb0bd5e764d1f55c7e32067847
Hoooon22 <momo990305@gmail.com>
2021-03-23 15:59:34 +0900

    make Record

---
+++ 9_property/Record/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Record
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
