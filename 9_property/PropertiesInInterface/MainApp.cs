96267bca23232dc37866ef220705f7eac07c35f3
Hoooon22 <momo990305@gmail.com>
2021-03-25 21:28:23 +0900

    make PropertiesInInterface

---
+++ 9_property/PropertiesInInterface/MainApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PropertiesInInterface
+{
+    interface INamedValue
+    {
+        string Name
+        {
+            get;
+            set;
+        }
+
+        string Value
+        {
+            get;
+            set;
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
