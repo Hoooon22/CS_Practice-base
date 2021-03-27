9810ab047f217fd4757c6009a22f9320903e4251
Hoooon22 <momo990305@gmail.com>
2021-03-27 15:26:31 +0900

    mkdir 10 and make ArraySample.cs

---
+++ 9_property/ArraySample/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArraySample
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
