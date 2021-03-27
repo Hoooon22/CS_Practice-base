9f9f270f7c7f215989fe41969f62a9315f359667
Hoooon22 <momo990305@gmail.com>
2021-03-27 15:06:07 +0900

    create abstract class

--- 9_property/PropertiesInAbstractClass/MainApp.cs
+++ 9_property/PropertiesInAbstractClass/MainApp.cs
@@ -2,11 +2,35 @@
 
 namespace PropertiesInAbstractClass
 {
+    abstract class Product
+    {
+        private static int serial = 0;
+        public string SerialID
+        {
+            get {return String.Format("{0:5d}", serial++);}
+        }
+
+        abstract public DateTime Productdate
+        {
+            get;
+            set;
+        }
+    }
+
+    class Myproduct : Product
+    {
+        public override DateTime Productdate
+        {
+            get;
+            set;
+        }
+    }
+
     class MainApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
