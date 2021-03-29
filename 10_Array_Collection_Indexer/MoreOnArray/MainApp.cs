6891f0cc097ce46e21d759ba0e51eccf81d25930
Hoooon22 <momo990305@gmail.com>
2021-03-29 14:32:41 +0900

    MoreOnArray

--- 10_Array_Collection_Indexer/MoreOnArray/MainApp.cs
+++ 10_Array_Collection_Indexer/MoreOnArray/MainApp.cs
@@ -4,9 +4,38 @@ namespace MoreOnArray
 {
     class MainApp
     {
+        private static bool CheckPassed(int score)
+        {
+            return score >= 60;
+        }
+
+        private static void Print(int value)
+        {
+            Console.Write($"{value} ");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] scores = new int[] { 80, 74, 81, 90, 34 };
+
+            foreach (int score in scores)
+                Console.Write($"{score} ");
+            Console.WriteLine();
+
+            Array.Sort(scores);
+            Array.ForEach<int>(scores, new Action<int>(Print));
+            Console.WriteLine();
+
+            Console.WriteLine($"Number of dimensions : {scores.Rank}");
+
+            Console.WriteLine($"Binary Search :  81 is at " +
+                $"{Array.BinarySearch<int>(scores, 81)}");
+
+            Console.WriteLine($"Linear Search :  90 is at " +
+                $"{Array.IndexOf<int>(scores, 90)}");
+
+            Console.WriteLine($"EveryOne passed ? : " +
+                $"{Array.TrueForAll<int>(scores, CheckPassed)}");
         }
     }
 }
