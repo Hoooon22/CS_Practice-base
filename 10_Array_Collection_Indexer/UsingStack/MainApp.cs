e92e03ffe9e6cb36fa950f45f2648c5b46308c9a
Hoooon22 <momo990305@gmail.com>
2021-03-29 16:31:50 +0900

    UsingStack

--- 10_Array_Collection_Indexer/UsingStack/MainApp.cs
+++ 10_Array_Collection_Indexer/UsingStack/MainApp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace UsingStack
 {
@@ -6,7 +7,15 @@ namespace UsingStack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            while (stack.Count > 0)
+                Console.WriteLine(stack.Pop());
         }
     }
 }
