6ad488094135237ef648f066f79d42855f775379
Hoooon22 <momo990305@gmail.com>
2021-03-29 14:42:09 +0900

    make Slice

---
+++ 10_Array_Collection_Indexer/Slice/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Slice
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
