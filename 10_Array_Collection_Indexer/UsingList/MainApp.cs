c578c17fdf5d201960abc37523fc9aefa18e0a0c
Hoooon22 <momo990305@gmail.com>
2021-03-29 15:41:46 +0900

    make UsingList.cs

---
+++ 10_Array_Collection_Indexer/UsingList/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UsingList
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
