f003147d07aad32824ee3ed1fcc84fdf4445e31e
Hoooon22 <momo990305@gmail.com>
2021-03-29 14:58:29 +0900

    make 2DArray.cs

---
+++ 10_Array_Collection_Indexer/2DArray/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2DArray
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
