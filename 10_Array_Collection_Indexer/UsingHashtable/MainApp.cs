300b844380c9b916ec2e1839e247f2789a9785af
Hoooon22 <momo990305@gmail.com>
2021-03-30 17:41:22 +0900

    UsingHashtable

--- 10_Array_Collection_Indexer/UsingHashtable/MainApp.cs
+++ 10_Array_Collection_Indexer/UsingHashtable/MainApp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace UsingHashtable
 {
@@ -6,7 +7,10 @@ namespace UsingHashtable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Hashtable ht = new Hashtable();
+            ht["하나"] = "one";
+
+            Console.WriteLine(ht["하나"]);
         }
     }
 }
