a61332f934516cde2d96b6678c2f5cabb059d7a0
Hoooon22 <momo990305@gmail.com>
2021-04-03 18:13:12 +0900

    InitializingCollections

--- 10_Array_Collection_Indexer/InitializingCollections/MainApp.cs
+++ 10_Array_Collection_Indexer/InitializingCollections/MainApp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using static System.Console;
 
 namespace InitializingCollections
 {
@@ -6,7 +8,27 @@ namespace InitializingCollections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 123, 456, 789 };
+
+            ArrayList list = new ArrayList(arr);
+            foreach (object item in list)
+                WriteLine($"ArrayList : {item}");
+            WriteLine();
+
+            Stack stack = new Stack(arr);
+            foreach (object item in stack)
+                WriteLine($"Stack : {item}");
+            WriteLine();
+
+            Queue queue = new Queue(arr);
+            foreach (object item in queue)
+                WriteLine($"Queue : {item}");
+            WriteLine();
+
+            ArrayList list2 = new ArrayList() { 11, 22, 33 }; // 컬렉션 초기자를 이용한 컬렉션 초기화
+            foreach (object item in list2)
+                WriteLine($"Arraylist2 : {item}");
+            WriteLine();
         }
     }
 }
