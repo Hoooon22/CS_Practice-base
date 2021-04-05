e771c05d36f0f5b228d21d70a330dcaf501572aa
Hoooon22 <momo990305@gmail.com>
2021-04-05 18:03:43 +0900

    make CopyingArray cs

---
+++ 11_Generalization/CopyingArray/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CopyingArray
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
