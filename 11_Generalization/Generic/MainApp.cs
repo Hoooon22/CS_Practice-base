4c8dea25052fcd2da0eb85ebc97155cfd5f75014
Hoooon22 <momo990305@gmail.com>
2021-04-06 17:36:08 +0900

    make Generic cs

---
+++ 11_Generalization/Generic/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generic
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
