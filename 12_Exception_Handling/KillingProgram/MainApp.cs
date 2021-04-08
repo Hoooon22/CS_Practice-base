84cbbe27fa5e125b193da28bb9c377110800fa73
Hoooon22 <momo990305@gmail.com>
2021-04-08 21:10:59 +0900

    make KillingProgram cs

---
+++ 12_Exception_Handling/KillingProgram/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KillingProgram
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
