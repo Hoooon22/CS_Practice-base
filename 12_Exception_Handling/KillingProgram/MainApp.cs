a236bafa14e8dd0a184f9309e5616aba37355302
Hoooon22 <momo990305@gmail.com>
2021-04-08 21:12:34 +0900

    KillingProgram

--- 12_Exception_Handling/KillingProgram/MainApp.cs
+++ 12_Exception_Handling/KillingProgram/MainApp.cs
@@ -6,7 +6,12 @@ namespace KillingProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 1, 2, 3 };
+
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(arr[i]);
+
+            Console.WriteLine("종료");
         }
     }
 }
