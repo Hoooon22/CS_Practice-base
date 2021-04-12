6447c671c32ebc7f208d851bacd270d2f1d7062a
Hoooon22 <momo990305@gmail.com>
2021-04-12 19:37:28 +0900

    make MyException cs

---
+++ 12_Exception_Handling/MyException/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyException
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
