bd5ba509f433b44f43780b6a7247101d10293ecc
Hoooon22 <momo990305@gmail.com>
2021-04-14 15:52:56 +0900

    make UsingCallback cs

---
+++ 13_Delegate_Event/UsingCallback/MainApp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UsingCallback
+{
+    delegate int Compare(int a, int b);
+
+    class MainApp
+    {
+        static int AscendCompare(int a, int b)
+        {
+            if (a > b)
+                return 1;
+            else if (a == b)
+                return 0;
+            else
+                return -1;
+        }
+
+        static int DescendCompare(int a, int b)
+        {
+            if (a < b)
+                return 1;
+            else if (a == b)
+                return 0;
+            else
+                return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }   
+}
