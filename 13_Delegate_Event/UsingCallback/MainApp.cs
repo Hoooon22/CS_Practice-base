fde2b7f9a9c9d6bddb5e1d71847e3fafe54571d9
Hoooon22 <momo990305@gmail.com>
2021-04-19 18:27:12 +0900

    UsingCallback

--- 13_Delegate_Event/UsingCallback/MainApp.cs
+++ 13_Delegate_Event/UsingCallback/MainApp.cs
@@ -26,6 +26,25 @@ namespace UsingCallback
                 return -1;
         }
 
+        static void BubbleSort(int[] Dataset, Compare Comparer)
+        {
+            int i = 0;
+            int j = 0;
+            int temp = 0;
+
+            for (i = 0; i < Dataset.Length-1; i++)
+            {
+                for (j = 0; j < Dataset.Length - (i+1); j++)
+                {
+                    if (Comparer(Dataset[j], Dataset[j+1]) > 0)
+                    {
+                        temp = Dataset[j + 1];
+
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
