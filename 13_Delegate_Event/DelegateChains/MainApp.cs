c007eb02c991dcc5100591c7d9ba2b7d379fb2fd
Hoooon22 <momo990305@gmail.com>
2021-04-26 20:14:38 +0900

    create DelegateChains.cs

---
+++ 13_Delegate_Event/DelegateChains/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelegateChains
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
