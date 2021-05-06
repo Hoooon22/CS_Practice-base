792042bcb52d124f59161b88ea6d58e6a878f01b
Hoooon22 <momo990305@gmail.com>
2021-05-06 21:26:26 +0900

    sync

--- 20_WinForm/UsingControls/MainApp.Designer.cs
+++ 20_WinForm/UsingControls/MainApp.Designer.cs
@@ -29,10 +29,17 @@ namespace UsingControls
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "MainApp";
+            this.Text = "Control Test";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
