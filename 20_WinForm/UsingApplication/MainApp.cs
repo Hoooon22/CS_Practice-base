5b357af9544f8e78c86ed8bf9c4b26fbcbd1c46c
Hoooon22 <momo990305@gmail.com>
2021-05-06 14:48:52 +0900

    UsingApplication

---
+++ 20_WinForm/UsingApplication/MainApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UsingApplication
+{
+    class MyForm : System.Windows.Forms.Form
+    {
+    }
+
+    class MainApp : Form
+    {
+        static void Main(string[] args)
+        {
+            MyForm form = new MyForm();
+
+            // 윈도우를 클릭하면 Application.Exit()를 호출
+            form.Click += new EventHandler(
+                (sender, eventArgs) =>
+                {
+                    Console.WriteLine("Closing Window...");
+                    Application.Exit();
+                });
+
+            Console.WriteLine("Starting Window Application...");
+            Application.Run(form);
+
+            Console.WriteLine("Exiting Window Application...");
+        }
+    }
+}
