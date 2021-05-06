399c88d133062c560f5f7fa7fa70e1120c880752
Hoooon22 <momo990305@gmail.com>
2021-05-06 17:16:26 +0900

    create FormEvent

---
+++ 20_WinForm/FormEvent/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FormEvent
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
